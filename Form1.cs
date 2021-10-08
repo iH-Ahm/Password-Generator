6c7a8a7c1f02464887aba92a615a23592c146dc0
iH-Ahm <36645426+iH-Ahm@users.noreply.github.com>
2021-10-08 18:27:23 +0100

    Update Form

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace Password_Generator
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
@@ -21,8 +23,51 @@ namespace Password_Generator
         {
             if (checkBox1.Checked == true)
             {
-
+                textBox1.Text = getUpperCase();
             }
+        }
+
+        private string getUpperCase()
+        {
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return letters[random.Next(0,letters.Length)].ToString();
+        }
+
+        private string getLowerCase()
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            return letters[random.Next(0, letters.Length)].ToString();
+        }
+
+        private string getNumber()
+        {
+            string numbers = "0123456789";
+            return numbers[random.Next(0, numbers.Length)].ToString();
+        }
+
+        private string getSymbol()
+        {
+            string symbols = "!£$%^&*()-_+=";
+            return symbols[random.Next(0, symbols.Length)].ToString();
+        }
+
+        private string generatePassword()
+        {
+            int passwordLength = Convert.ToInt32(numericUpDown1.Value);
+            string password = "";
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                string selectedValue = selectValue();
+                password = password + selectedValue;
+            }
+
+            return password;
+        }
+
+        private string selectValue()
+        {
+            return "";
         }
     }
 }
